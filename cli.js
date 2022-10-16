3b2234623a3d58e3df6834bdb7a006b8829b9825
AT-290690 <88512646+AT-290690@users.noreply.github.com>
2022-10-16 09:56:43 +0300

    Update cli.js

--- cli.js
+++ cli.js
@@ -1,5 +1,8 @@
 import { encodeUrl } from './language/misc/compression.js'
 import { readFileSync } from 'fs'
+import { interpred } from './language/misc/fs.js'
+
 export const encode = path => encodeUrl(readFileSync(path, 'utf8'))
 export const link = path =>
   `https://at-290690.github.io/toy_lang/index.html?s=${encode(path)}`
+export const evaluate = path => interpred(readFileSync(path, 'utf8'))
